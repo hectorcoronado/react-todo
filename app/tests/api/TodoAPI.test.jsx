daab57bcc387d476b5109438231fb4dccc699421
hectorcoronado <coronado.hector@gmail.com>
2016-09-26 13:00:23 -0700

    Filtered todos by showCompleted, tests written for it

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -35,7 +35,6 @@ describe('TodoAPI', () => {
 
       expect(localStorage.getItem('todos')).toBe(null);
     });
-
   });
 
   describe('getTodos', () => {
@@ -60,8 +59,38 @@ describe('TodoAPI', () => {
 
       expect(actualTodos).toEqual(todos);
     });
-
   });
 
+  describe('filterTodos', () => {
+    var todos = [
+      {
+        id: 1,
+        text: 'Some random text',
+        completed: true
+      },
+      {
+        id: 2,
+        text: 'Other random text',
+        completed: false
+      },
+      {
+        id: 3,
+        text: 'More random text',
+        completed: true
+      }
+    ];
+
+    it('should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return non-completed todos if showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filteredTodos.length).toBe(1);
+    })
+  });
 
 });
