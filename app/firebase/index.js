39dc1c29ec1b00f7255ebebb5b1f1b57c186ad98
hectorcoronado <coronado.hector@gmail.com>
2016-10-04 09:33:28 -0700

    Removed Firebase credentials from GitHub repo

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,11 +3,11 @@ import firebase from 'firebase';
 // Initialize Firebase
 try {
   var config = {
-    apiKey: "AIzaSyCO9sodfunweBbFtobS81Udeh0QQVlVNOA",
-    authDomain: "react-todo-app-44c67.firebaseapp.com",
-    databaseURL: "https://react-todo-app-44c67.firebaseio.com",
-    storageBucket: "react-todo-app-44c67.appspot.com",
-    messagingSenderId: "235231655695"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config);
 } catch (err) {
